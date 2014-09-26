d17af86bb4d2c68feeab830a16b0025d66ef61ed
James Doc <jamesdoc@gmail.com>
2014-09-26 13:27:34 +0100

    Adding in 'build a tweet'

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ import jinja2
 
 from roomlookup import ROOMLOOKUPDICT
 
+WORKSHOP = 15
+SPECIAL_EVENT = 24
 EVENING_TALK_CODE = 40
-MEMBERSHIP_EVENT_CODE = 45
 DAILY_TOUR_CODE = 41
+BSL_TALK = 42
+SEMINAR = 44
+MEMBERSHIP_EVENT_CODE = 45
+
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -32,8 +37,9 @@ def get_first_int_in_list(src_list):
 class MainHandler(webapp2.RequestHandler):
     ''' prints to screen the cal
     '''
+
     def get(self):
-        for daycount in range(0,28):
+        for daycount in range(0,6):
             scandate = (datetime.now() + days_delta(daycount)).strftime("%Y%m%d")
             self.response.write('<h2>%s</h2>' % scandate )
             urlpath = "http://www.vam.ac.uk/whatson/json/events/day/%s/" % scandate
@@ -62,6 +68,72 @@ class MainHandler(webapp2.RequestHandler):
                 self.response.write('<hr>')
 
 
+class EventHandler(webapp2.RequestHandler):
+    def get_events_on_date(self, scandate):
+        urlpath = "http://www.vam.ac.uk/whatson/json/events/day/%s/" % scandate
+        response = urllib2.urlopen(urlpath)
+        data = json.load(response)
+        return data
+
+    def filter_irrelevant_events(self, events):
+        relevant = []
+        for d in events:
+            if d['fields']['event_type'] in [WORKSHOP, SPECIAL_EVENT, EVENING_TALK_CODE, BSL_TALK, SEMINAR, MEMBERSHIP_EVENT_CODE]:
+                relevant.append(d)
+        return relevant
+
+    def prioritise_relevant_events(self, events):
+        for i, d in enumerate(events):
+
+            if 'Friday Late' in d['fields']['name']:
+                events.insert(0, events.pop(i))
+
+        return events
+
+    def write_tweet(self, event):
+        # print 'Name: %s' % event['fields']['name'].encode('utf8')
+        # print 'Event type: %s' % event['fields']['event_type']
+        # print 'Desc: %s' % event['fields']['short_description'].encode('utf8')
+        # print '#'*16
+
+        def add_ordinal(day):
+            if day[-1] == '1':
+                return '%sst' % day
+            elif day[-1] == '2':
+                return '%snd' % day
+            elif day[-1] == '3':
+                return '%srd' % day
+
+            return '%sth' % day
+
+        def strip_leading_zero(day):
+            return day.lstrip('0')
+
+        event_datetime = datetime.strptime(event['fields']['last_slot'], '%Y-%m-%d %H:%M:%S')
+        display_day = add_ordinal(strip_leading_zero(event_datetime.strftime('%d')))
+        display_month = event_datetime.strftime('%b')
+        display_time = event_datetime.strftime('%H:%M')
+        display_url = 'http://www.vam.ac.uk/whatson/event/%s' % event['pk']
+
+        tweet = '%s %s %s: %s | %s' % (display_day, display_month, display_time, event['fields']['name'].encode('utf8'), display_url)
+
+        return tweet
+
+    def get(self):
+        for daycount in range(0, 7):
+            scandate = (datetime.now() + days_delta(daycount)).strftime("%Y%m%d")
+            events = self.get_events_on_date(scandate)
+            events = self.filter_irrelevant_events(events)
+            events = self.prioritise_relevant_events(events)
+            try:
+                event = events[0]
+                tweet = self.write_tweet(event)
+                self.response.write(tweet)
+                self.response.write('<hr>')
+            except(IndexError):
+                pass
+
+
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
@@ -70,6 +142,7 @@ class HomeHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
+    ('/events', EventHandler),
     ('/cal', MainHandler),
     ('.*', HomeHandler)
 ], debug=True)
